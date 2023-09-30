63dcaf1e3c5fa1e742267ae47987a02016970c61
SilvaFilipe <silva.filipef@gmail.com>
2023-09-30 22:10:02 +0100

    chore: bulma & design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-link to="/">Notes</router-link> |
-  <router-link to="/stats">Stats</router-link>
-  <router-view />
+  <navbar />
+
+  <div class="container is-max-desktop w-100 px-2 py-4">
+    <router-view />
+  </div>
 </template>
+
+<script setup>
+import Navbar from '@/components/layout/navbar.vue'
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/components/layout/navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">Notecubes</div>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          @click.prevent="showMobileNav = !showMobileNav"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+        <div class="navbar-end">
+          <router-link class="navbar-item" active-class="is-active" to="/"> Notes </router-link>
+          <router-link class="navbar-item" active-class="is-active" to="/stats">
+            Stats
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showMobileNav = ref(false)
+</script>
+
+<style>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    width: 100%;
+    left: 0;
+  }
+}
+</style>
